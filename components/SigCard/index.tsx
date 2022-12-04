e66f689f8d7e151b10591c480b8f773d1d5d0d37
Chaitanya <chaitanya.anand@gmail.com>
2022-12-05 01:23:34 +0530

    about page still working

--- components/SigCard/index.tsx
+++ components/SigCard/index.tsx
@@ -5,9 +5,9 @@ type Props = {};
 
 const SigCard = (props: Props) => {
   return (
-    <div className="flex rounded-xl flex-col items-center border-white border-4">
-      <img src="https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3=w240-h480-rw" alt="sig-pic" className="rounded-full w-2/3" />
-      <h3 className="text-2xl font-medium">{props.sigName}</h3>
+    <div className="flex rounded-xl sig-bg flex-col items-center p-4 ">
+      <img src='./assets/images/website-icon.png' alt="sig-pic" className="rounded-full px-3  pt-3" />
+      <h3 className="text-2xl font-medium py-5">{props.sigName}</h3>
     </div>
   );
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SigCard from "../components/SigCard";
 
 import Layout from "../components/Layout";
+import Faq from "../components/Faq";
 
 type Props = {};
 
@@ -9,9 +10,17 @@ const About = (props: Props) => {
   
   return (
     <Layout>
-      <section className="mx-36">
-        <h1 className="text-7xl font-bold">What Are SIGs?</h1>
-        <p className="text-2xl my-8 font-poppins">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis in molestiae asperiores alias hic ut facere perspiciatis nisi similique, commodi dolores. Commodi ducimus optio nemo doloremque? Recusandae eius nulla esse? Eum, ducimus! Placeat eveniet exercitationem alias modi qui, libero, minus fuga quidem necessitatibus ea, atque vitae esse rerum. Veniam error natus expedita deleniti eveniet nobis sunt eligendi neque. Natus totam mollitia odio, dolore ratione blanditiis perferendis debitis consectetur obcaecati maiores!</p>
+
+
+    <Faq/>
+
+
+
+      {/* overflow hatana hai, horizontal scrollbar hatana hai */}
+      {/* img extend hori usse fix krna hai */}
+      <div className="absolute mr-4 px-36  pt-14  z-10 ">
+      <h1 className="text-7xl font-bold">What Are SIGs?</h1>
+        <p className="text-xl my-8 font-poppins">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis in molestiae asperiores alias hic ut facere perspiciatis nisi similique, commodi dolores. Commodi ducimus optio nemo doloremque? Recusandae eius nulla esse? Eum, ducimus! Placeat eveniet exercitationem alias modi qui, libero, minus fuga quidem necessitatibus ea, atque vitae esse rerum. Veniam error natus expedita deleniti eveniet nobis sunt eligendi neque. Natus totam mollitia odio, dolore ratione blanditiis perferendis debitis consectetur obcaecati maiores!</p>
 
         <div className="grid grid-cols-5 gap-x-7 ">
         <SigCard sigName = "Development"/>
@@ -20,9 +29,17 @@ const About = (props: Props) => {
         <SigCard sigName = "Finance"/>
         <SigCard sigName = "Data Analysis"/>
         </div>
+      </div>
+      <div className="h-full w-full absolute z-[2] bg-[#1f0909]/[0.5]"></div>
+      <img
+        src='/assets/images/sig-bg.jpg'
+        alt="SIG Background Image"
+        className=" object-cover     mt-7 rounded-lg"
+      />
 
 
-      </section>
+
+
     </Layout>
   );
 };
