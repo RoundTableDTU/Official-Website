4e63b0f7ad29fd39d56c53b614ac4c56416a0b63
Chaitanya <chaitanya.anand@gmail.com>
2022-12-04 23:16:32 +0530

    navbar done

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div className="w-full px-4 grid grid-rows-2 gap-10 md:flex md:justify-between items-center text-xl md:text-2xl py-4 shadow-neon-orange-primary md:gap-0 rounded-sm">
+    <div className="w-full px-4 grid grid-rows-2 gap-10 md:flex md:justify-between items-center text-xl md:text-2xl py-4  md:gap-0 rounded-sm main-navbar">
       <div className="flex justify-center items-center md:flex gap-6 ">
         <div>
           <Image
@@ -16,15 +16,15 @@ const Navbar = (props: Props) => {
             alt="round table logo png"
           />
         </div>
-        <h1 className="text-2xl md:text-3xl">
-          Round table: The --- Society of DTU
+        <h1 className="text-2xl md:text-3xl underline font-bold">
+          RoundTable
         </h1>
       </div>
-      <div className="w-full md:w-1/3 flex justify-evenly p-4 md:p-0">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/about"}>About</Link>
-        <Link href={"/team"}>Team</Link>
-        <Link href={"/events"}>Events</Link>
+      <div className="w-full md:w-1/3 flex justify-evenly text-xl p-4 font-poppins font-normal md:p-0">
+        <Link className="py-3 px-5" href='/'>Home</Link>
+        <Link className="   py-3 px-5" href={"/about"}>About</Link>
+        <Link className="active-page py-3 px-5" href={"/team"}>Team</Link>
+        <Link className=" py-3 px-5" href={"/events"}>Events</Link>
       </div>
     </div>
   );
