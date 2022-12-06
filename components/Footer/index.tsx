f5819eda30bccc376bda964830ce4bf6f0cb723a
Chaitanya <chaitanya.anand@gmail.com>
2022-12-07 00:55:59 +0530

    homepage+footer+team final

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,52 +1,90 @@
 
 
 import React from "react";
+import Image from "next/image";
+
 
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <div>
-    <footer className=" bg-gradient-to-r from-[#EC9F05] to-[#FF4E00] z-[-10] ">
+  return <div className="bg-[#0A0702] relative -top-2 pt-12 md:pt-20">
+    <footer className=" bg-gradient-to-r  to-primary-orange from-[#F04512] z-[-10] ">
 
       <div className="text-center">
-        <span className="mx-auto  rounded-md top-0 border font-semibold left-5 content-center items-center text-black text-2xl md:text-4xl font-poppins p-6 bg-white w-1/2">
+        <span className="mx-auto   rounded-md top-0 border font-semibold left-5 md:content-center items-center text-black text-base md:text-3xl font-poppins p-6 lg:px-28 bg-white md:w-2/3">
           Embark upon an exciting journey.
         </span>
       </div>
 
-      <div className="relative bottom-10  pt-32 md:pt-40  text-white">
-        <div className="flex flex-col justify-around md:flex-row mb-4  ml-10 ">
-          <div className="mb-8">
-            <h1 className="font-poppins font-bold text-xl md:text-4xl md:mb-4">Roundtable</h1>
-            <div className="md:text-2xl">
-              Roundtable,<br />
-              Delhi Technological University, <br />
-              New Delhi
+      <div className="relative bottom-10  pt-28 md:pt-32 md:pb-1  text-white">
+        <div className="grid grid-cols-1 md:grid-cols-4  md:justify-start items-start content-start   ">
+          <div className="px-10 pb-4">
+            <h1 className="font-poppins font-bold text-xl md:text-3xl md:mb-4">Roundtable</h1>
+            <div className="md:text-xl">
+              The Official Skill Development Society,<br />
+              Delhi Technological University<br />
+              
             </div>
           </div>
-          <div className="mb-8">
-            <h1 className="font-poppins font-bold text-xl md:text-4xl md:mb-4">Quick Access</h1>
-            <span className="mr-2 md:text-2xl"><a href="#Events">Events</a></span>
-            <span className="m-2 md:text-2xl"><a href="#Events">Events</a></span>
-            <span className="m-2 md:text-2xl"><a href="#Events">Events</a></span>
-            <span className="m-2 md:text-2xl"><a href="#Events">Events</a></span>
+          <div className="px-10 py-4 md:py-0">
+            <h1 className="font-poppins font-bold text-xl md:text-3xl md:mb-4">Quick Access</h1>
+
+            <div className="md:text-xl">
+            <span className="mr-2 "><a href="#Events">Home</a></span>
+            <span className="m-2 "><a href="#Events">About</a></span>
+            <span className="m-2 "><a href="#Events">Events</a></span>
+            <span className="m-2 "><a href="#Events">Team</a></span>
+            </div>
+          </div>
+          <div className="px-10 py-4 md:py-0">
+            <h1 className="font-poppins font-bold text-xl md:text-3xl">Follow Us</h1>
+            <div className="flex flex-row gap-4 py-3 font-semibold">
+
+            <img
+                src="/assets/logos/insta.png"
+                
+                alt="insta logo"
+                className="h-10 md:h-12 text-white cursor-pointer"
+              />
+            <img
+                src="/assets/logos/ln.png"
+                
+                alt="linkedin logo"
+                className="h-10 md:h-12 text-white cursor-pointer"
+              />
+            <img
+                src="/assets/logos/fb.png"
+                
+                alt="facebook logo"
+                className="h-10 md:h-12 text-white cursor-pointer"
+              />
+
+            </div>
+
 
           </div>
-          <div className="mb-1">
-            <h1 className="font-poppins font-bold text-xl md:text-4xl">Follow Us</h1>
 
+          <div className="px-10">
+            
+          <h1 className="font-poppins font-bold text-xl  md:text-3xl">Contact Us</h1>
+            <h4 className="py-1 md:text-xl">Saksham Sharma (President) <span className="block">9013844884</span></h4>
+            <h4 className="py-1 md:text-xl">Saksham Gupta (Vice President) <span className="block">9643717655</span></h4>
           </div>
         </div>
       </div>
     </footer>
 
-    <div className="flex flex-col items-center md:flex-row md:justify-between">
-      <div>
-        © Copyright RoundtableDTU. <span className="font-bold">All Rights Reserved</span>
+    <div className="flex flex-col py-5 font-semibold px-10 md:text-lg md:pb-6 text-white md:items-center md:flex-row md:justify-between">
+      <div className="">
+        © Copyright RoundtableDTU. 
       </div>
-      <div>
+      <div className="pt-3 pb-5">
+      All <span className="text-primary-orange ">Rights</span> Reserved.
+      </div>
+      
+      {/* <div>
         Designed by <span className="text-primary-orange">Roundtable</span>
-      </div>
+      </div> */}
 
     </div>
   </div>;
--- utility/roundtableText.ts
+++ utility/roundtableText.ts
@@ -23,13 +23,14 @@ export const slides = [
       image: `/assets/images/carouselImages/a1.jpeg`,
       title: `ROUNDTABLE.`,
       mediumTitle: `WORKSHOPS`,
-      text: `We organize Technical and Non-Technical workshops for members and students These include resume building, case study analysis, technical workshops such as competitive programming, web development & machine learning.`,
+      text: `    We organise Technical and Non-Technical workshops for our members and other students.
+These include sessions on resumé building, case study analysis and finance and for tech students we have workshops on competitive programming, web dev and machine learning.`,
     },
   ];
 
   export const accordianText = [
     {
-      title: `What is RoundTable DTU about?`,
+      title: `What is RoundTable DTU?`,
       content: `Round Table, Delhi Technological University was formed in the summer of 2014 with the aim of knitting together the technical and non-technical aspects of a job.
 
       Society strives to inculcate the essential skills required for technical and non-technical job profiles. We aim to bridge the gap between the numerous aspects of a job and make students market-ready. Our vision is achieved by engaging students in various workshops, highlighting the nuances of CV building, financing and so on.
@@ -37,26 +38,28 @@ export const slides = [
       Our team is always open to working with enthusiastic and talented young people who are diligent in their endeavors.`,
     },
     {
-      title: `Info About Events and Competitions :`,
+      title: `What Events and Competitions are organized by us?`,
       content: `We conduct regular events & competitions to challenge our skill sets.
+      We also have our own yearly Aurora fest which includes various interesting events and prizes for students.
+      Also every year a tech fest called Invictus with some other societies is also organized which is DTU's biggest tech fest.
       We organize regular meetups, group discussions, case study competitions, and hackathons for students.`,
     },
     {
-      title: `What kind of Workshops are organized ?`,
+      title: `What kind of Workshops are organized?`,
       content: `We organize Technical and Non-Technical workshops for members and students
       
       These include resume building, case study analysis, technical workshops such as competitive programming, web development & machine learning.`,
     },
     {
-      title: `Skills and Knowledge Development Sessions :`,
+      title: `What about skills and knowledge development?`,
       content: `We organize special sessions conducted by people who are industry leaders. These are interactive sessions that involve career guidance, current trends & insights into their fields.`,
     },
     {
-      title: `Who can join?`,
-      content: `All the students of DTU can join society. All you need to do is fill the recruitment form when it rolls out and follow our social media handles for information regarding recruitments.`,
+      title: `Who can join RoundTable?`,
+      content: `All the students of DTU who are enthusiast enough can join society. All you need to do is fill the recruitment form when it rolls out and follow our social media handles for information regarding recruitments.`,
     },
     {
-      title: `Different departments :`,
+      title: `What are the different departments in RoundTable`,
       content: `Graphic
       Designing
       Web Development
@@ -69,7 +72,7 @@ export const slides = [
       Management Roles`,
     },
     {
-      title: `Why should you join RoundTable DTU :`,
+      title: `Why should you join RoundTable DTU?`,
       content: `The most important thing you need to do in college is to explore as much as you can. Try out Coding, Video Editing, and all the fields that interest you.
 
       That is exactly where RoundTable DTU will help you. As there are so many departments you are free to explore and try out your options.
@@ -84,16 +87,21 @@ export const slides = [
       So there are a variety of webinars that are conducted in our society.`,
     },
     {
-      title: `Social Media :`,
+      title: `How to get updates on what's happening in RoundTable?`,
       content: `On our Instagram and LinkedIn page we post informative content related to Tech and Non Tech topics.
       On our Instagram page, we also post regular updates regarding multiple events happening in the society.
       
       All of our Informative Webinars are live streamed on YouTube and you can check them out whenever you want. Check out all our social media handles here`,
     },
     {
-      title: `Who can join?`,
-      content: `All the students of DTU can join society. All you need to do is fill the recruitment form when it rolls out and follow our social media handles for information regarding recruitments.`,
+      title: `What are SIGs?`,
+      content: `SIGs are special interest groups which the members of the society can become a part of. Here we help members to learn the skills and apply them by assigning them assignments.
+      We offer various SIGs for both tech and non-tech like DSA, AI-ML, Web Dev, Consulting and Analytics, Product Management, Finance, Stock Trading.`,
     },    
+    {
+      title: 'Why RoundTable is better than other societies?',
+      content: 'RoundTable DTU is the official Skill Development Society of DTU and it is also recognized by DTU administration. The society will help you build your resume by allowing you participate and organise multiple events and competitions. Joining Our Society will help you network effectively with your seniors and other students from DTU.'
+    },
   ]
 
   // about page text
