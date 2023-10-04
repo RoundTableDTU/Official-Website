75b3534096cf70152ae9ffb2acc8cffc40cd552c
RajKunx2 <113442064+RajKunx2@users.noreply.github.com>
2023-10-04 10:33:07 +0530

    footer icons

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -9,8 +9,8 @@ const Footer = (props: Props) => {
     <footer className=" bg-gradient-to-r  to-primary-orange from-[#F04512] z-[-10] ">
 
       <div className="text-center">
-        <span className="mx-auto   rounded-md top-0 border font-semibold left-5 md:content-center items-center text-black text-base md:text-3xl font-poppins p-6 lg:px-28 bg-orange-500 md:w-2/3">
-          Embark upon an exciting journey.
+        <span className="mx-auto rounded-md top-0 border font-semibold left-5 md:content-center items-center text-black text-base md:text-3xl font-poppins p-6 lg:px-28 bg-orange-500 md:w-2/3">
+          <i className="text-white font-semibold">Embark upon an exciting journey</i>
         </span>
       </div>
 
@@ -28,36 +28,38 @@ const Footer = (props: Props) => {
             <h1 className="font-poppins font-bold text-xl md:text-3xl md:mb-4 ">Quick Access</h1>
 
             <div className="md:text-xl flex flex-col md:flex-row">
-              <span className="m-2 "><Link href={'/'}>Home</Link></span>
-              <span className="m-2 "><Link href={'/about'}>About</Link></span>
-              <span className="m-2 "><Link href={'/events'}>Events</Link></span>
-              <span className="m-2 "><Link href={'/projects'}>Projects</Link></span>
-              <span className="m-2 "><Link href={'/team'}>Team</Link></span>
+              <span className="my-2 md:mr-6"><Link href={'/'}>Home</Link></span>
+              <span className="my-2 md:mr-6"><Link href={'/about'}>About</Link></span>
+              <span className="my-2 md:mr-6"><Link href={'/events'}>Events</Link></span>
+              </div>
+              <div className="md:text-xl flex flex-col md:flex-row flex-start">
+              <span className="my-2 md:basis-1/3 md:mr-6"><Link href={'/projects'}>Projects</Link></span>
+              <span className="my-2 md:basis-1/3 md:mr-6"><Link href={'/team'}>Team</Link></span>
             </div>
           </div>
           <div className="px-10 py-4 md:py-0">
             <h1 className="font-poppins font-bold text-xl md:text-3xl">Follow Us</h1>
             <div className="flex flex-row gap-4 py-3 font-semibold">
               <Link href={'https://www.instagram.com/roundtable_dtu/'}>
-                <Image width={50} height={40}
+                <Image width={50} height={50}
                   src="/assets/logos/insta.png"
 
                   alt="insta logo"
-                  className="h-12 md:h-12 text-white cursor-pointer"
+                  className="h-12 text-white cursor-pointer"
                 /></Link>
               <Link href={'https://www.linkedin.com/company/round-table-dtu/'}>
-                <Image width={50} height={40}
+                <Image width={50} height={50}
                   src="/assets/logos/ln.png"
 
                   alt="linkedin logo"
-                  className="h-10 md:h-12 text-white cursor-pointer"
+                  className="h-12 text-white cursor-pointer"
                 /></Link>
               <Link href={'https://www.facebook.com/roundtabledtu/'}>
-                <Image width={50} height={40}
+                <Image width={50} height={50}
                   src="/assets/logos/fb.png"
 
                   alt="facebook logo"
-                  className="h-10 md:h-12 text-white cursor-pointer"
+                  className="h-12 text-white cursor-pointer"
                 /></Link>
 
             </div>
