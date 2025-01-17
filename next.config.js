376d2f52d3561b0e5eae8d6bb183f16b0f16df2e
RoundTableDTU <roundtabledtu@gmail.com>
2025-01-17 10:52:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,3 +2,10 @@
 module.exports = {
   reactStrictMode: true,
 }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // ...your other config options
+}
+
+module.exports = nextConfig
